4d33a3d6ef432bbe00de41b2e6bf80307b4f6afe
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-02-26 19:57:42 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,35 @@ import "./style.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello, I'm an excuses generator");
 };
 
-who = ["Your mother", "Spiderman", "My grandma", "Peter Griffin"];
+let who = [
+  "Your mother",
+  "Spiderman",
+  "My grandma",
+  "Peter Griffin",
+  "Their brother",
+  "The dog"
+];
+let action = ["ate", "peed", "crushed", "broke", "kissed"];
+let what = ["my homework", "my computer", "the car", "a sandwich", "an axe"];
+let when = [
+  "before the class",
+  "right now",
+  "while I was working out",
+  "during my lunch",
+  "before going to sleep"
+];
+let where = ["in the park", "in the bathroom", "in my room", "in the church"];
+
+function getRandomExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  let randomWhere = where[Math.floor(Math.random() * where.length)];
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen} ${randomWhere}`;
+}
+
+document.getElementById("excuse").innerHTML = getRandomExcuse();
